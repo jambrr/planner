70a9e9d6d9e29842c8e78816e46e366f2580d616
jambrr <louis.weber99@gmail.com>
2022-10-18 09:26:43 +0200

    parser

--- planner/src/background.js
+++ planner/src/background.js
@@ -1,21 +1,27 @@
 'use strict';
+import * as chrono from 'chrono-node';
 
 // With background scripts you can communicate with popup
 // and contentScript files.
 // For more information on background script,
 // See https://developer.chrome.com/extensions/background_pages
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
+function getSelectedText(info){
+    console.log(info.selectionText);
+    let data = chrono.parseDate(info.selectionText);
+    console.log(data);
 
-    // Log message coming from the `request` parameter
-    console.log(request.payload.message);
-    // Send a response message
-    sendResponse({
-      message,
-    });
-  }
+    if(data != null){
+        chrome.tabs.create({url: "https://www.google.com"});
+    }
+}
+chrome.contextMenus.create({
+    id: "1",
+    title: "test",
+    contexts: ["selection"],
 });
+
+chrome.contextMenus.onClicked.addListener(function(info, tab){
+    getSelectedText(info)
+});
+
